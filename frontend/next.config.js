ebf75429fcc0c9ca8e40bab752c9f065339d338a
NiemHuu <nguyenhuuniem12022005@gmail.com>
2025-11-17 00:11:55 +0700

    rửewrewr

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,35 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3001',
-        pathname: '/**', // Cho phép tất cả đường dẫn từ backend
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
       {
         protocol: 'https',
         hostname: 'p-market.onrender.com',
-        pathname: '/uploads/**',
+        pathname: '/uploads/**'
       },
       {
         protocol: 'https',
         hostname: 'p-market-1.onrender.com',
-        pathname: '/uploads/**', // phòng trường hợp ảnh nằm ngay frontend
+        pathname: '/uploads/**'
       },
-    ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**'
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
